6cf59c43b032c0a88ddb43bf84716c562adadab5
Witcher26 <extreme8igor088@mail.ru>
2023-01-05 17:12:38 +0300

    простейший калькулятор на JavaScript

--- Calculator.js
+++ Calculator.js
@@ -1,2 +1,29 @@
-Dfdfd
-//просто файл ёпть
+const OPERATIONS = {
+    sum: '+',
+    multiply: '*',
+    subscract: '-',
+    divide: '/'
+};
+
+function calculate({a, b, operation }) {
+    let result = null;
+
+    switch (operation) {
+        case OPERATIONS.sum:
+            result = sum(a,b);
+            break;
+        case OPERATIONS.subscract:
+            result = subscract(a, b);
+            break;
+        case OPERATIONS.multiply:
+            result = multiply(a, b);
+            break;
+        case OPERATIONS.divide:
+            result = divide(a, b);
+            break;
+
+        default:
+            break;
+    }
+    return result;
+}
